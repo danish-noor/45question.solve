6985b8386989b180f51b424d8cf316b33a2e1d8d
danish-noor <kinzadani00@gmail.com>
2024-04-21 20:59:55 +0500

    answer01

---
+++ q1.js
@@ -0,0 +1,8 @@
+//Question 1: Install Node.js, TypeScript and VS Code on your computer/Laptop.
+console.log("\t\n<==============MAKE A ENVIRONMENT FOR TYPESCRIPT ON MY LAPTOP==========================>\n\t");
+console.log("Installation Completed");
+var install = ["V.S CODE", "NODE.JS", "TYPESCRIPT"];
+for (var i = 0; i < install.length; i++) {
+    console.log(" ".concat(i + 1, " ").concat(install[i]));
+}
+;
---
+++ q1.ts
@@ -0,0 +1,10 @@
+//Question 1: Install Node.js, TypeScript and VS Code on your computer/Laptop.
+
+console.log("\t\n<==============MAKE A ENVIRONMENT FOR TYPESCRIPT ON MY LAPTOP==========================>\n\t");
+console.log("Installation Completed");
+const install:string[] = ["V.S CODE", "NODE.JS", "TYPESCRIPT"]; 
+
+for(let i = 0; i < install.length ; i++){
+      console.log(` ${i+1} ${install[i]}`);
+      
+};
