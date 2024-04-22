e005f53019a1649d18a09194eacca24653e0f22a
danish-noor <kinzadani00@gmail.com>
2024-04-23 02:49:54 +0500

    45QuestionSolve

---
+++ q3.ts
@@ -0,0 +1,9 @@
+//Q3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName1: string = "dAnIsH";
+
+console.log("print in LowerCase:" + personName1.toLowerCase());
+
+console.log("print in UpperCase:" + personName1.toUpperCase());
+
+console.log("print in TitleCase:" + personName1.charAt(0).toUpperCase() + personName1.slice(1).toLowerCase());
