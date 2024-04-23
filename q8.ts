f862de337a72a7dbc3a1d9a35fcdbf0f968c32ed
danish-noor <kinzadani00@gmail.com>
2024-04-23 20:59:58 +0500

    45QuestionSolve

---
+++ q8.ts
@@ -0,0 +1,10 @@
+//Q8.You should create four lines that look like this:
+
+// console.log(5 + 3)
+
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+console.log("Line Number One:",5 + 3);
+console.log("Line Number Two:",10 - 2);
+console.log("Line Number Three:",4 * 2);
+console.log("Line Number Four:",16 / 2);
---
+++ q9.ts
@@ -0,0 +1,7 @@
+//Q9.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoriteNumber: number = 8;
+
+let messageRevels: string = `My Favorite Number is: ${favoriteNumber}`;
+
+console.log(messageRevels);
