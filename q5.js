463fa8b5311f675b95007fb310e9848ecb735e87
danish-noor <kinzadani00@gmail.com>
2024-04-23 19:35:25 +0500

    45QuestionSolve

---
+++ q5.js
@@ -0,0 +1,5 @@
+//Q5.Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Albert Einstein";
+var famous_quote = "“A person who never made a mistake never tried anything new.”";
+var message = "".concat(famous_person, " once said, ").concat(famous_quote);
+console.log(message);
