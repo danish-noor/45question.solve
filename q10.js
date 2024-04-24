e4dd2c77c0990047c1c47f6917fcf28b2182160d
danish-noor <kinzadani00@gmail.com>
2024-04-24 10:02:59 +0500

    45QuestionSolve

---
+++ q10.js
@@ -0,0 +1,10 @@
+//Q10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//Danish Noor 2024/03/24
+var num1 = 10;
+var num2 = 20;
+// <====================Addition=================================>
+// this programe add two number and print the result. (this is comment in typescript)
+console.log(num1 + num2); //I predict the answer will be (30).
+// <====================Subtraction===============================>
+// this programe subtraction two number and print the result. (this is comment in typescript)
+console.log(num1 - num2); //I predict the answer will be (10).
---
+++ q12.js
@@ -0,0 +1,5 @@
+//Q12.Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var names = ["asad", "bilal", "danish", "irfan"];
+for (var i = 0; i < names.length; i++) {
+    console.log("Hello, ".concat(names[i], "! How are you today?"));
+}
