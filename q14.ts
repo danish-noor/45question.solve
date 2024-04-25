39fbbf0ec4d9fb2e9940e6acdf2aa77ff5637a5d
danish-noor <kinzadani00@gmail.com>
2024-04-25 22:41:15 +0500

    45QuestionSolve

---
+++ q14.ts
@@ -0,0 +1,9 @@
+//Q14.Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let guestList:string[] = ["asad","irfan","ali"];
+
+for(let i = 0; i < guestList.length; i++ ){
+    console.log(`Dear ${guestList[i]}, you invite to dinner.`);
+    
+
+}
---
+++ q15.js
@@ -0,0 +1,14 @@
+//Q15.Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+// • Print a second set of invitation messages, one for each person who is still in your list.
+var guestList = ["Asad", "Irfan", "Ali"];
+//print the name who can't come dinner
+var guestNotCome = guestList.splice(1, 1)[0];
+console.log("".concat(guestNotCome, " can't come for dinner.\n"));
+// push
+guestList.push("Danish");
+// print a message
+guestList.forEach(function (guest) {
+    console.log("Dear ".concat(guest, " you are invited."));
+});
